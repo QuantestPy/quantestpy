c556a383d4c2fa98f4273c90674d960fbc19cbec
junnaka51 <junnaka0112@gmail.com>
2022-08-19 01:41:58 +0000

    add 3 tests for cx gate

--- test/test_test_circuit_cx_gate.py
+++ test/test_test_circuit_cx_gate.py
@@ -68,6 +68,7 @@ class TestTestCircuitCXGate(unittest.TestCase):
             control_qubit=[0], target_qubit=[2], control_value=[1]
         )
 
+        # this is qiskit's output
         expected_gate = np.array([
             [1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j],
             [0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j],
@@ -94,3 +95,60 @@ class TestTestCircuitCXGate(unittest.TestCase):
 
         self.assertIsNone(
             np.testing.assert_allclose(actual_gate, expected_gate))
+
+    def test_toffoli(self,):
+        circ = TestCircuit(3)
+        actual_gate = circ._create_all_qubit_gate_from_cx_gate(
+            control_qubit=[0, 1], target_qubit=[2], control_value=[1, 1]
+        )
+
+        expected_gate = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
+                                  [0, 1, 0, 0, 0, 0, 0, 0],
+                                  [0, 0, 1, 0, 0, 0, 0, 0],
+                                  [0, 0, 0, 1, 0, 0, 0, 0],
+                                  [0, 0, 0, 0, 1, 0, 0, 0],
+                                  [0, 0, 0, 0, 0, 1, 0, 0],
+                                  [0, 0, 0, 0, 0, 0, 0, 1],
+                                  [0, 0, 0, 0, 0, 0, 1, 0]])
+
+        self.assertIsNone(
+            np.testing.assert_allclose(actual_gate, expected_gate))
+
+    def test_toffoli_qiskit_qubit_order(self,):
+        circ = TestCircuit(3)
+        circ._from_right_to_left_for_qubit_ids = True
+        actual_gate = circ._create_all_qubit_gate_from_cx_gate(
+            control_qubit=[0, 1], target_qubit=[2], control_value=[1, 1]
+        )
+
+        # this is qiskit's output
+        expected_gate = np.array([
+            [1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j],
+            [0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j],
+            [0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j],
+            [0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j],
+            [0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j],
+            [0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j],
+            [0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j],
+            [0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j]])
+
+        self.assertIsNone(
+            np.testing.assert_allclose(actual_gate, expected_gate))
+
+    def test_cx_multiple_targets(self,):
+
+        circ = TestCircuit(3)
+        gate_0 = circ._create_all_qubit_gate_from_cx_gate(
+            control_qubit=[0], target_qubit=[1, 2], control_value=[1]
+        )
+
+        circ = TestCircuit(3)
+        gate_1_0 = circ._create_all_qubit_gate_from_cx_gate(
+            control_qubit=[0], target_qubit=[1], control_value=[1]
+        )
+        gate_1_1 = circ._create_all_qubit_gate_from_cx_gate(
+            control_qubit=[0], target_qubit=[2], control_value=[1]
+        )
+
+        self.assertIsNone(
+            np.testing.assert_allclose(gate_0, np.matmul(gate_1_0, gate_1_1)))
