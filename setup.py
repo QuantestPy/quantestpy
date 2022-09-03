452f450102d3bfdc48c4222effacfa193b84fe50
junnaka51 <junnaka0112@gmail.com>
2022-09-03 07:33:13 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+setup(
+    name="quantestpy",
+    version="0.1.0",
+    license="Apache-2.0",
+    description="Test framework for quantum circuits",
+    author="mitz1012,junnaka51",
+    url="https://github.com/QuantestPy/quantestpy",
+    packages=find_packages(),
+    py_modules=[splitext(basename(path))[0]
+                for path in glob('quantestpy/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.8.0',
+    install_requires=_requires_from_file('requirements.txt')
+)
