ea9745182981fd0fb8971f2a1856c87b0aa1ae6c
junnaka51 <junnaka0112@gmail.com>
2022-09-06 00:42:40 +0000

    specify packages

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-from glob import glob
-from os.path import basename
-from os.path import splitext
-
 from setuptools import setup
-from setuptools import find_packages
 
 
 def _requires_from_file(filename):
@@ -17,9 +12,7 @@ setup(
     description="Software for testing quantum computing programs.",
     author="Quantestpy Development Team",
     url="https://github.com/QuantestPy/quantestpy",
-    packages=find_packages(),
-    py_modules=[splitext(basename(path))[0]
-                for path in glob("quantestpy/*.py")],
+    packages=["quantestpy"],
     include_package_data=True,
     zip_safe=False,
     python_requires=">=3.7",
