ebdf44eb551b31755e2a13b468e04cbe30ec67a6
junnaka51 <junnaka0112@gmail.com>
2022-11-05 02:28:26 +0000

    implement a new assert method assert_get_ctrl_val

--- quantestpy/unary_iteration.py
+++ quantestpy/unary_iteration.py
@@ -37,6 +37,35 @@ def _get_ctrl_val_of_all_ops_on_syst_reg_for_given_val_in_select_reg(
     return ctrl_val_of_all_ops
 
 
+def _get_qubit_idx_to_ctrl_val_for_given_val_in_select_reg(
+        val_in_select_reg: str,
+        ftc: FastTestCircuit,
+        select_reg: list,
+        ancilla_reg: list) -> dict:
+
+    # init ancilla reg to 0
+    ftc.set_qubit_value(ancilla_reg, [0] * len(ancilla_reg))
+    # init select reg to val_in_select_reg
+    ftc.set_qubit_value(select_reg, [int(i) for i in val_in_select_reg])
+
+    # get ctrl vals for all ops on syst reg
+    qubit_idx_to_ctrl_val = {idx: [] for idx in select_reg + ancilla_reg}
+    for i, gate in enumerate(ftc._gates):
+
+        ctrl_qubit_idx_for_one_op = gate["control_qubit"]
+        ctrl_qubit_val_for_one_op = \
+            ftc._qubit_value[ctrl_qubit_idx_for_one_op].tolist()
+
+        for j, ctrl_qubit_idx in enumerate(ctrl_qubit_idx_for_one_op):
+            if ctrl_qubit_idx in qubit_idx_to_ctrl_val.keys():
+                qubit_idx_to_ctrl_val[ctrl_qubit_idx].append(
+                    ctrl_qubit_val_for_one_op[j])
+
+        ftc._execute_i_th_gate(i)
+
+    return qubit_idx_to_ctrl_val
+
+
 def assert_check_ctrl_val_of_all_ops_on_syst_reg(
         circuit: FastTestCircuit,
         select_reg: list,
@@ -148,3 +177,52 @@ def assert_equal_ctrl_val_of_all_ops_on_syst_reg(
                     + f"when val in select reg is {bin_val_in_select_reg}."
                 msg = ut_test_case._formatMessage(msg, err_msg)
                 raise QuantestPyAssertionError(msg)
+
+
+def assert_get_ctrl_val(
+        circuit: FastTestCircuit,
+        select_reg: list,
+        ancilla_reg: list = [],
+        assert_is_ancilla_uncomputated: bool = False,
+        verbose: bool = True) -> dict:
+
+    # check inputs
+    FastTestCircuit._assert_is_fast_test_circuit(circuit, "Input circuit")
+    ftc = copy.deepcopy(circuit)
+    ftc._assert_is_correct_reg(select_reg, "select_reg")
+    ftc._assert_is_correct_reg(ancilla_reg, "ancilla_reg")
+
+    len_select_reg = len(select_reg)
+    qubit_idx_to_val_in_select_reg_to_ctrl_val = \
+        {idx: dict() for idx in select_reg + ancilla_reg}
+
+    for dec_val_in_select_reg in range(2**len_select_reg):
+
+        bin_val_in_select_reg = \
+            ("0" * len_select_reg + bin(dec_val_in_select_reg)[2:])[
+                -len_select_reg:]
+
+        qubit_idx_to_ctrl_val = \
+            _get_qubit_idx_to_ctrl_val_for_given_val_in_select_reg(
+                bin_val_in_select_reg,
+                ftc,
+                select_reg,
+                ancilla_reg
+            )
+
+        if assert_is_ancilla_uncomputated:
+            if not np.all(ftc._qubit_value[ancilla_reg] == 0):
+                err_msg = "ancilla reg is not uncomputated to 0 " \
+                    + f"when val in select reg is {bin_val_in_select_reg}."
+                raise QuantestPyAssertionError(err_msg)
+
+        if verbose:
+            print(f"val in select reg: {bin_val_in_select_reg},",
+                  f"qubit idx to ctrl val: {qubit_idx_to_ctrl_val}")
+
+        for qubit_idx, ctrl_val in qubit_idx_to_ctrl_val.items():
+            tmp_dict = qubit_idx_to_val_in_select_reg_to_ctrl_val[qubit_idx]
+            tmp_dict[bin_val_in_select_reg] = ctrl_val
+            qubit_idx_to_val_in_select_reg_to_ctrl_val[qubit_idx] = tmp_dict
+
+    return qubit_idx_to_val_in_select_reg_to_ctrl_val
---
+++ test/test_unary_iteration_get_qubit_idx_to_ctrl_val.py
@@ -0,0 +1,167 @@
+import unittest
+
+from quantestpy import FastTestCircuit
+from quantestpy.unary_iteration import \
+    _get_qubit_idx_to_ctrl_val_for_given_val_in_select_reg as get_ctrl_val
+
+
+class TestUnaryIterGetQubitIdxToCtrlVal(unittest.TestCase):
+    """
+    How to execute this test:
+    $ pwd
+    {Your directory where you git-cloned quantestpy}/quantestpy
+    $ python -m unittest test.test_unary_iteration_get_qubit_idx_to_ctrl_val
+    .
+    ----------------------------------------------------------------------
+    Ran 1 test in 0.001s
+
+    OK
+    $
+    """
+
+    def setUp(self) -> None:
+        """
+        This is the circuit in Figure 7 in arxiv:1805.03662
+        """
+        self.ftc = FastTestCircuit(1+4+11+4)
+        self.ftc.add_gate({"name": "x", "target_qubit": [16],
+                           "control_qubit": [0, 1], "control_value": [1, 0]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [17],
+                           "control_qubit": [16, 2], "control_value": [1, 0]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [18],
+                           "control_qubit": [17, 3], "control_value": [1, 0]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18, 4], "control_value": [1, 0]})
+        # X_0
+        self.ftc.add_gate({"name": "y", "target_qubit": [5],
+                           "control_qubit": [19], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18], "control_value": [1]})
+        # X_1
+        self.ftc.add_gate({"name": "y", "target_qubit": [6],
+                           "control_qubit": [19], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18, 4], "control_value": [1, 1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [18],
+                           "control_qubit": [17], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18, 4], "control_value": [1, 0]})
+        # X_2
+        self.ftc.add_gate({"name": "y", "target_qubit": [7],
+                           "control_qubit": [19], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18], "control_value": [1]})
+        # X_3
+        self.ftc.add_gate({"name": "y", "target_qubit": [8],
+                           "control_qubit": [19], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18, 4], "control_value": [1, 1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [18],
+                           "control_qubit": [17, 3], "control_value": [1, 1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [17],
+                           "control_qubit": [16], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [18],
+                           "control_qubit": [17, 3], "control_value": [1, 0]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18, 4], "control_value": [1, 0]})
+        # X_4
+        self.ftc.add_gate({"name": "y", "target_qubit": [9],
+                           "control_qubit": [19], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18], "control_value": [1]})
+        # X_5
+        self.ftc.add_gate({"name": "y", "target_qubit": [10],
+                           "control_qubit": [19], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18, 4], "control_value": [1, 1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [18],
+                           "control_qubit": [17], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18, 4], "control_value": [1, 0]})
+        # X_6
+        self.ftc.add_gate({"name": "y", "target_qubit": [11],
+                           "control_qubit": [19], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18], "control_value": [1]})
+        # X_7
+        self.ftc.add_gate({"name": "y", "target_qubit": [12],
+                           "control_qubit": [19], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18, 4], "control_value": [1, 1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [18],
+                           "control_qubit": [17, 3], "control_value": [1, 1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [17],
+                           "control_qubit": [16, 2], "control_value": [1, 1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [16],
+                           "control_qubit": [0], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [18],
+                           "control_qubit": [16, 3], "control_value": [1, 0]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18, 4], "control_value": [1, 0]})
+        # X_8
+        self.ftc.add_gate({"name": "y", "target_qubit": [13],
+                           "control_qubit": [19], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18], "control_value": [1]})
+        # X_9
+        self.ftc.add_gate({"name": "y", "target_qubit": [14],
+                           "control_qubit": [19], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [19],
+                           "control_qubit": [18, 4], "control_value": [1, 1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [18],
+                           "control_qubit": [16], "control_value": [1]})
+        # X_10
+        self.ftc.add_gate({"name": "y", "target_qubit": [15],
+                           "control_qubit": [18], "control_value": [1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [18],
+                           "control_qubit": [16, 3], "control_value": [1, 1]})
+        self.ftc.add_gate({"name": "x", "target_qubit": [16],
+                           "control_qubit": [0, 1], "control_value": [1, 1]})
+
+        self.select_reg = [0, 1, 2, 3, 4]
+        self.system_reg = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        self.ancilla_reg = [16, 17, 18, 19]
+
+    def tearDown(self) -> None:
+        del self.ftc
+
+    def test_regular(self,):
+        val_in_select_reg_to_ctrl_val = {
+            "10000": {0: [1, 1, 1],
+                      1: [0, 0],
+                      2: [0, 0],
+                      3: [0, 0, 0, 0, 0, 0],
+                      4: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      16: [1, 1, 1, 0, 0, 0],
+                      17: [1, 1, 1, 0, 0, 0],
+                      18: [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      19: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+            "11001": {0: [1, 1, 1],
+                      1: [1, 1],
+                      2: [0, 0],
+                      3: [0, 0, 0, 0, 0, 0],
+                      4: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                      16: [0, 0, 0, 1, 1, 1],
+                      17: [0, 0, 0, 0, 0, 0],
+                      18: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+                      19: [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]},
+            "11010": {0: [1, 1, 1],
+                      1: [1, 1],
+                      2: [0, 0],
+                      3: [1, 1, 1, 1, 1, 1],
+                      4: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      16: [0, 0, 0, 1, 1, 1],
+                      17: [0, 0, 0, 0, 0, 0],
+                      18: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                      19: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+        }
+        for val_in_select_reg, expect_ctrl_val in \
+                val_in_select_reg_to_ctrl_val.items():
+
+            actual_ctrl_val = get_ctrl_val(
+                val_in_select_reg=val_in_select_reg,
+                ftc=self.ftc,
+                select_reg=self.select_reg,
+                ancilla_reg=self.ancilla_reg
+            )
+            self.assertDictEqual(expect_ctrl_val, actual_ctrl_val)
