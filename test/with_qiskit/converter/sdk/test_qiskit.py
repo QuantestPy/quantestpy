18b997cad0408d88b3643dfa63e8794d5a431762
mitz1012 <34256508+mitz1012@users.noreply.github.com>
2023-02-04 13:24:04 +0900

    change the error test using self.assertRaises(QuantestPyError)

--- test/with_qiskit/converter/sdk/test_qiskit.py
+++ test/with_qiskit/converter/sdk/test_qiskit.py
@@ -1,4 +1,3 @@
-import traceback
 import unittest
 
 import numpy as np
@@ -29,18 +28,13 @@ class TestQiskit(unittest.TestCase):
         qc = QuantumCircuit(3)
         qc.rxx(np.pi/4, 0, 1)
 
-        try:
+        expected_error_msg = \
+            "Qiskit gate [rxx] is not supported in QuantestPy.\n" \
+            + f'Implemented qiskit gates: {_IMPLEMENTED_QISKIT_GATES}'
+
+        with self.assertRaises(QuantestPyError) as cm:
             qpc = cvt_input_circuit_to_quantestpy_circuit(qc)
             svc = cvt_quantestpy_circuit_to_state_vector_circuit(qpc)
             return svc
 
-        except QuantestPyError as e:
-
-            expected_error_msg = \
-                "quantestpy.exceptions.QuantestPyError: " \
-                + "Qiskit gate [rxx] is not supported in QuantestPy.\n" \
-                + f'Implemented qiskit gates: {_IMPLEMENTED_QISKIT_GATES}\n'
-
-            actual_error_msg = traceback.format_exception_only(type(e), e)[0]
-            print(actual_error_msg)
-            self.assertEqual(expected_error_msg, actual_error_msg)
+        self.assertEqual(cm.exception.args[0], expected_error_msg)
