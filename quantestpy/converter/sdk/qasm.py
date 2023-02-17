17b68f8533b865bf2a892bd59fea6ffb1754cd06
kotamanegi <kotamanegi84@gmail.com>
2023-02-17 04:37:23 +0000

    .

--- quantestpy/converter/sdk/qasm.py
+++ quantestpy/converter/sdk/qasm.py
@@ -1,17 +1,16 @@
 from quantestpy import QuantestPyCircuit
-from quantestpy.converter.sdk.qiskit import (
-    _cvt_qiskit_to_quantestpy_circuit, _raise_error_if_not_qiskit_installed)
+from quantestpy.converter.sdk.qiskit import _cvt_qiskit_to_quantestpy_circuit
+from quantestpy.exceptions import QuantestPyError
 
 try:
     from qiskit import QuantumCircuit
 
 except ModuleNotFoundError:
-    pass
-
-
-def _cvt_openqasm_to_quantestpy_circuit(qasm: str) -> QuantestPyCircuit:
-
-    _raise_error_if_not_qiskit_installed()
-
-    qiskit_circuit = QuantumCircuit.from_qasm_str(qasm)
-    return _cvt_qiskit_to_quantestpy_circuit(qiskit_circuit)
+    def _cvt_openqasm_to_quantestpy_circuit(qasm: str) -> QuantestPyCircuit:
+        raise QuantestPyError(
+            "Qiskit is missing. Please install it."
+        )
+else:
+    def _cvt_openqasm_to_quantestpy_circuit(qasm: str) -> QuantestPyCircuit:
+        qiskit_circuit = QuantumCircuit.from_qasm_str(qasm)
+        return _cvt_qiskit_to_quantestpy_circuit(qiskit_circuit)
