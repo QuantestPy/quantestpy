30bed72f409442b6ff41cf32f187f9575678cdc1
kotamanegi <kotamanegi84@gmail.com>
2023-02-17 11:46:38 +0900

    .

--- quantestpy/converter/converter_to_quantestpy_circuit.py
+++ quantestpy/converter/converter_to_quantestpy_circuit.py
@@ -2,6 +2,9 @@ from quantestpy import QuantestPyCircuit
 from quantestpy.converter.sdk.qasm import _cvt_openqasm_to_quantestpy_circuit
 from quantestpy.converter.sdk.qiskit import (
     _cvt_qiskit_to_quantestpy_circuit, _is_instance_of_qiskit_quantumcircuit)
+from quantestpy.converter.sdk.quriparts import (
+    _cvt_quri_parts_circuit_to_quantestpy_circuit,
+    _is_instance_of_quri_parts_quantumcircuit)
 from quantestpy.exceptions import QuantestPyError
 
 
@@ -18,6 +21,9 @@ def cvt_input_circuit_to_quantestpy_circuit(circuit) -> QuantestPyCircuit:
     # qiskit.QuantumCircuit()
     elif _is_instance_of_qiskit_quantumcircuit(circuit):
         quantestpy_circuit = _cvt_qiskit_to_quantestpy_circuit(circuit)
+
+    elif _is_instance_of_quri_parts_quantumcircuit(circuit):
+        quantestpy_circuit = _cvt_quri_parts_circuit_to_quantestpy_circuit(circuit)
 
     else:
         raise QuantestPyError(
